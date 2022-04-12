01adf50d5220894d6bcc5749873c248241f5f109
SOunit <workbookwork7@gmail.com>
2022-04-12 10:47:40 -0700

    add eslint setting

--- 03_eslint_prettier/01-color-button/src/App.test.js
+++ 03_eslint_prettier/01-color-button/src/App.test.js
@@ -20,7 +20,7 @@ test("button has correct initial color", () => {
   fireEvent.click(colorButton);
 
   expect(colorButton).toHaveStyle({ backgroundColor: "MidnightBlue" });
-  expect(colorButton.textContent).toBe("Change to Medium Violet Red");
+  expect(colorButton).toHaveTextContent("Change to Medium Violet Red");
 });
 
 test("initial conditions", () => {
