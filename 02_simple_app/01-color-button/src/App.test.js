8188b3004c1fba18e1f171216c7a12632d660067
SOunit <workbookwork7@gmail.com>
2022-04-11 17:15:16 -0700

    add test sample

--- 02_simple_app/01-color-button/src/App.test.js
+++ 02_simple_app/01-color-button/src/App.test.js
@@ -1,6 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
+// unit test have 1 assertion
+// functional test has multiple assertions
 test("button has correct initial color", () => {
   // test steps
   // 1. render
@@ -11,5 +13,10 @@ test("button has correct initial color", () => {
 
   // 3. create assertion
   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  expect(colorButton.textContent).toBe("Change to red");
 });
-test("button turns blue when clicked", () => {});
