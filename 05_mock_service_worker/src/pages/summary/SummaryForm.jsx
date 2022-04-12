986ccaa2d28794e1942a696fd69bfb1b705f0cca
SOunit <workbookwork7@gmail.com>
2022-04-12 14:35:09 -0700

    add new project

---
+++ 05_mock_service_worker/src/pages/summary/SummaryForm.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Popover from "react-bootstrap/Popover";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+
+// const Example = () => (
+
+// );
+
+// render(<Example />);
+
+const SummaryForm = () => {
+  const [tcChecked, setTcChecked] = useState(false);
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
+
+  const checkboxLabel = (
+    <span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}>Terms and Conditions</span>
+      </OverlayTrigger>
+    </span>
+  );
+
+  const clickCheckboxHandler = (event) => {
+    setTcChecked(event.target.checked);
+  };
+
+  return (
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={clickCheckboxHandler}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  );
+};
+
+export default SummaryForm;
